89d8f373db40caa8ed1d3ac5d1392e1ec2f8ca67
Luis M <luisricardo494@gmail.com>
2021-10-10 17:55:37 -0400

    test

---
+++ src/DisplayData.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const QUERY_ALL_USERS = gql`
+ query GetAllUsers {
+    users {
+     id
+     name
+     age
+     username
+    }
+ 
+ }
+`
+
+const DisplayData = () => {
+    const {data} = useQuery(QUERY_ALL_USERS);
+
+    if (data) {
+        console.log(data);
+    }
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DisplayData
