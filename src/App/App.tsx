fce99bbe1d6e4f1b50f5c1f69d782fa5bdd4389a
luisricardo494 <luisricardo494@gmail.com>
2021-10-10 13:30:17 -0400

    setting up ApolloClient

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import {ApolloClient, InMemoryCache, ApolloProvider, useQuery} from '@apollo/client'
 import './App.css';
 
 function App() {
+  //cache data is similar to state management but wtih apollo
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "http://localhost:3001/graphql"
+  })
   return (
     <div className="App">
-        app
+      <ApolloProvider client={client}>
+        <div>test test</div>
+      </ApolloProvider>
     </div>
   );
 }
